9ca2562274db95e7dd150055f7032ae9a9ce0a4c
Kieran Heese <kheese@Kierans-MacBook-Pro.local>
2019-10-14 18:01:39 -0400

    Reformat state list

--- site/sign_up.php
+++ site/sign_up.php
@@ -47,15 +47,6 @@
 									      <input type="text" name="last" id="last" value="" placeholder="Last Name" />
 									    </div>
 									    <!-- Break -->
-									    <div class="col-12">
-									      <select name="demo-category" id="demo-category">
-										<option value="">-Which product are you interested in? -</option>
-										<option value="1">The Simple Starter Kit - A coop, seed, and hatchlings </option>
-										<option value="1">Renewal - Seed and some hatchlings </option>
-										<option value="1">Bi-monthly Delivery - Seed and hatchlings  </option>
-										<option value="1">Monthly Delivery - Seed </option>
-									      </select>
-									    </div>
 									    <!-- Break -->
 									    <div class="col-12">
 									      <input type="text" name="email" id="email" value="" placeholder="Email"</input>
@@ -76,7 +67,60 @@
 										</div>
 									    <!-- Break -->
 										<div class="col-6 col-12-xsmall">
-										  <input type="text" name="state" id="state" value="" placeholder="State" />
+										  <select name="state-category" id="state-category">
+										    <option value="">Which state are you from?</option>
+										    <option value="AL">Alabama</option>
+										    <option value="AK">Alaska</option>
+										    <option value="AZ">Arizona</option>
+										    <option value="AR">Arkansas</option>
+										    <option value="CA">California</option>
+										    <option value="CO">Colorado</option>
+										    <option value="CT">Connecticut</option>
+										    <option value="DE">Delaware</option>
+										    <option value="DC">District Of Columbia</option>
+										    <option value="FL">Florida</option>
+										    <option value="GA">Georgia</option>
+										    <option value="HI">Hawaii</option>
+										    <option value="ID">Idaho</option>
+										    <option value="IL">Illinois</option>
+										    <option value="IN">Indiana</option>
+										    <option value="IA">Iowa</option>
+										    <option value="KS">Kansas</option>
+										    <option value="KY">Kentucky</option>
+										    <option value="LA">Louisiana</option>
+										    <option value="ME">Maine</option>
+										    <option value="MD">Maryland</option>
+										    <option value="MA">Massachusetts</option>
+										    <option value="MI">Michigan</option>
+										    <option value="MN">Minnesota</option>
+										    <option value="MS">Mississippi</option>
+										    <option value="MO">Missouri</option>
+										    <option value="MT">Montana</option>
+										    <option value="NE">Nebraska</option>
+										    <option value="NV">Nevada</option>
+										    <option value="NH">New Hampshire</option>
+										    <option value="NJ">New Jersey</option>
+										    <option value="NM">New Mexico</option>
+										    <option value="NY">New York</option>
+										    <option value="NC">North Carolina</option>
+										    <option value="ND">North Dakota</option>
+										    <option value="OH">Ohio</option>
+										    <option value="OK">Oklahoma</option>
+										    <option value="OR">Oregon</option>
+										    <option value="PA">Pennsylvania</option>
+										    <option value="RI">Rhode Island</option>
+										    <option value="SC">South Carolina</option>
+										    <option value="SD">South Dakota</option>
+										    <option value="TN">Tennessee</option>
+										    <option value="TX">Texas</option>
+										    <option value="UT">Utah</option>
+										    <option value="VT">Vermont</option>
+										    <option value="VA">Virginia</option>
+										    <option value="WA">Washington</option>
+										    <option value="WV">West Virginia</option>
+										    <option value="WI">Wisconsin</option>
+										    <option value="WY">Wyoming</option>
+										  </select>
 										</div>
 										<div class="col-6 col-12-xsmall">
 										  <input type="text" name="zip" id="zip" value="" placeholder="Zip Code" />
