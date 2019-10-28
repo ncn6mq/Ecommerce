0309c48968df7947ae89d936f6ab57c944058ee1
Kieran Heese <kheese@d-172-25-203-191.dhcp.virginia.edu>
2019-10-28 14:34:56 -0400

    Trial add email functionality.

--- site/contact_us.php
+++ site/contact_us.php
@@ -3,7 +3,78 @@
 	Editorial by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+  -->
+
+<?php
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\OAuth;
+// Alias the League Google OAuth2 provider class
+use League\OAuth2\Client\Provider\Google; 
+
+
+if (array_key_exists('user-message', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+    require '../vendor/autoload.php';
+
+    //Create a new PHPMailer instance;
+    $mail = new PHPMailer;
+
+    //Tell PHPMailer to use SMTP - requires a local mail server;
+    //Faster and safer than using mail();
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = 'simple.eggs.biz@gmail.com';
+    //Password to use for SMTP authentication
+    $mail->Password = 'CS4753Ecommerce';
+    //Set who the message is to be sent from
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('simple.eggs.biz@gmail.com', 'Simple Eggs');
+    //Send the message to yourself, or whoever should receive contact for submissions
+    $mail->addAddress('simple.eggs.biz@gmail.com', 'Simple Eggs');
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    $mail->addReplyTo('simple.eggs.biz@gmail.com', 'Simple Eggs2'); 
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Category: {$_POST['issue-category']}
+Username (if any): {$_POST['username']}
+Message: {$_POST['user-message']}
+EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but you shouldn't display errors to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
+?>
+
+
+?>
+
 <html>
 	<head>
 		<title>SimplEggs - Contact Us</title>
@@ -36,35 +107,31 @@
 										<h1>Contact Us</h1>
 									</header>
 									<h3>Send us a message</h3>
-									<form method="post" action="#">
+									<form method="post" action="contact_us.php">
 									  <div class="row gtr-uniform">
 									    <div class="col-4 col-12-xsmall">
-									      <input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+									      <input type="text" name="name" id="name" value="" placeholder="Name" />
 									    </div>
 									    <div class="col-4 col-12-xsmall">
-									      <input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+									      <input type="email" name="email" id="email" value="" placeholder="Email" />
 									    </div>
 									    <div class="col-4 col-12-xsmall">
-									      <input type="text" name="demo-username" id="demo-username" value="" placeholder="Username (If Any)" />
+									      <input type="text" name="username" id="username" value="" placeholder="Username (If Any)" />
 									    </div>
 									    <!-- Break -->
 									    <div class="col-12">
-									      <select name="demo-category" id="demo-category">
-										<option value="">- What is the nature of this issue? -</option>
-										<option value="1">Potential Customer Question</option>
-										<option value="1">Missed/wrong delivery</option>
-										<option value="1">How can we improve your customer experience</option>
-										<option value="1">Other</option>
+									      <select name="issue-category" id="issue-category">
+										<option value="other">- What is the nature of this issue? -</option>
+										<option value="potential-customer">Potential Customer Question</option>
+										<option value="wrong-delivery">Missed/wrong delivery</option>
+										<option value="customer-experience">How can we improve your customer experience</option>
+										<option value="other">Other</option>
 									      </select>
 									    </div>
 									    <!-- Break -->
-									    <div class="col-6 col-12-small">
-									      <input type="checkbox" id="demo-copy" name="demo-copy">
-									      <label for="demo-copy">Email me a copy of this message</label>
-									    </div>
 									    <!-- Break -->
 									    <div class="col-12">
-									      <textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+									      <textarea name="user-message" id="user-message" placeholder="Enter your message" rows="6"></textarea>
 									    </div>
 									    <!-- Break -->
 									    <div class="col-12">
