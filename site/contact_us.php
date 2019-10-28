2502dc7ea152331261a0ead528af77b1a4e5b444
Kieran Heese <kheese@Kierans-MacBook-Pro.local>
2019-10-28 16:56:44 -0400

    Add PHPMailer Zip file

--- site/contact_us.php
+++ site/contact_us.php
@@ -9,9 +9,9 @@
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\OAuth;
+//use PHPMailer\PHPMailer\OAuth;
 // Alias the League Google OAuth2 provider class
-use League\OAuth2\Client\Provider\Google; 
+//use League\OAuth2\Client\Provider\Google; 
 
 
 if (array_key_exists('user-message', $_POST)) {
@@ -70,9 +70,6 @@ EOT;
         $msg = 'Invalid email address, message ignored.';
     }
 }
-?>
-
-
 ?>
 
 <html>
