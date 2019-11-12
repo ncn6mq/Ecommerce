ceb9bf313f4b8c65b6894a6b291fbb2051b28194
notzane <zane@alpher.com>
2019-11-11 19:19:19 -0500

    crossed out btc price as well

--- site/member_page.php
+++ site/member_page.php
@@ -159,7 +159,7 @@ $final_cost_subscription = substr($str_cost2, 0, 5);
 											<h2>Chicken Coop</h2>
 										</header>
 										<p>We will deliver the materials and instructions straight to you. All you need is a hammer, screwdriver, and a few hours to set it up.</p>
-										<p><b><?php echo ($valid_code ? '<del>$250</del> $200' : '$250' ) ?> or <?php echo ($valid_code ? $final_cost_one_time_discount : $final_cost_one_time ) ?> BTC</b></p>
+										<p><b><?php echo ($valid_code ? '<del>$250</del> $200' : '$250' ) ?> or <?php echo ($valid_code ? '<del>' . $final_cost_one_time . '</del> ' . $final_cost_one_time_discount : $final_cost_one_time ) ?> BTC</b></p>
 										
 										<form action="https://test.bitpay.com/checkout" method="post">
 											<input type="hidden" name="action" value="checkout" />
